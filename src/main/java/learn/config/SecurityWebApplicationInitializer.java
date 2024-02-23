b17c9bd97e4ea09d197f0ae265fdf1700cb1e890
QuangIDR <nguyenduyquangk0@gmail.com>
2024-02-23 17:01:41 +0700

    deleted trash

--- src/main/java/learn/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-//package learn.config;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-//
-//}
